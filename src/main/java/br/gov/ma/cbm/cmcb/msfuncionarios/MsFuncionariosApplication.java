9342513423d95a8bcf28b3970e9c8be2e0e75abf
Unidade I <dinizdcmcb@gmail.com>
2021-02-03 15:08:37 -0300

    Projeto ms-funcionarios criado

---
+++ src/main/java/br/gov/ma/cbm/cmcb/msfuncionarios/MsFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ma.cbm.cmcb.msfuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ma/cbm/cmcb/msfuncionarios/MsFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ma.cbm.cmcb.msfuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
